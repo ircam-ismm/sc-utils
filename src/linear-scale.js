85107bd6e9ab13c9e8227f77b610be8fe463c250
b-ma <benjamin.matuszewski@gmail.com>
2024-07-14 13:30:51 +0200

    fix: harmonize argument naming

--- src/linear-scale.js
+++ src/linear-scale.js
@@ -1,10 +1,10 @@
 /**
  * Create a scale function
- * @param {number} minIn - Minimum input
- * @param {number} maxIn - Maximum input
- * @param {number} minOut - Minimum output
- * @param {number} maxOut - Maximum output
- * @param {boolean} [clamp=false] - Clamp output
+ * @param {number} inputStart - Start value of input range
+ * @param {number} inputEnd - End value of input range
+ * @param {number} outputStart - Start value of output range
+ * @param {number} outputEnd - End value of output range
+ * @param {boolean} [clip=false] - Clip output to output range
  * @return {Function}
  * @example
  * import { scale } from '@ircam/sc-utils';
@@ -12,14 +12,15 @@
  * myScale(0.5);
  * // > 75
  */
-export function linearScale(minIn, maxIn, minOut, maxOut, clamp = false) {
-  const a = (maxOut - minOut) / (maxIn - minIn);
-  const b = minOut - a * minIn;
-  if (!clamp) {
+export function linearScale(inputStart, inputEnd, outputStart, outputEnd, clip = false) {
+  const a = (outputEnd - outputStart) / (inputEnd - inputStart);
+  const b = outputStart - a * inputStart;
+
+  if (!clip) {
     return x => a * x + b;
   } else {
-    const upperBound = Math.max(minOut, maxOut);
-    const lowerBound = Math.min(minOut, maxOut);
+    const upperBound = Math.max(outputStart, outputEnd);
+    const lowerBound = Math.min(outputStart, outputEnd);
 
     return x => {
       const y = a * x + b;
