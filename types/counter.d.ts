7c47dd0efe0ceac6304fed03311e22c85e05bcc7
jipodine <jipodine@users.noreply.github.com>
2026-01-21 10:49:47 +0100

    docs: rebuild

--- types/counter.d.ts
+++ types/counter.d.ts
@@ -5,14 +5,13 @@
  * @param {number} [to=Number.MAX_SAFE_INTEGER] - End of the counter, included
  * @param {number} [step=1] - Increment / decrement step, if 0 returns `from` forever
  * @returns {Function}
- *
+ * @example
  * import { counter } from '@ircam/sc-utils';
  * const myCounter = counter(0.1, 0.3, 0.1);
  * counter(); // 0.1
  * counter(); // 0.2
  * counter(); // 0.3
  * counter(); // 0.1
- * // ...
  */
 export function counter(from?: number, to?: number, step?: number): Function;
 //# sourceMappingURL=counter.d.ts.map
