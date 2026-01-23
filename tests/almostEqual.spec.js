e017832537916fd7ad5ae2d0cc4ee23a28072106
jipodine <jipodine@users.noreply.github.com>
2026-01-23 18:12:53 +0100

    almostEqual: test inequality for array

--- tests/almostEqual.spec.js
+++ tests/almostEqual.spec.js
@@ -19,18 +19,18 @@ describe('almostEqual', () => {
         (value, tolerance) => {
 
           assert(almostEqual(value, value, tolerance),
-            `Same value must be equal for any tolerance: value: ${value}, tolerance: ${tolerance}`
+            `Same value must be equal for any tolerance: value: ${value}, tolerance: ${tolerance}`,
           );
 
           // tolerance must sum all possible computation errors
           const range = Math.max(Math.abs(value), Math.abs(tolerance));
 
           assert(almostEqual(value, value + Math.sign(value) * tolerance, 2 * tolerance),
-            `Absolute tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`
+            `Absolute tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
           );
 
           assert(almostEqual(value, value * range, 2 * range),
-            `Relative tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`
+            `Relative tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
           );
 
           const differentValue = Math.sign(value) * Math.max(
@@ -41,15 +41,13 @@ describe('almostEqual', () => {
             && Math.abs(differentValue) !== Infinity
           ) {
             assert(!almostEqual(value, differentValue, 0.5 * range),
-              `Tolerance too relaxed: value: ${value}, tolerance: ${tolerance}, range: ${range}`
+              `Tolerance too relaxed: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
             );
           }
 
         }), {
         ...debugOptions,
-      },
-    );
-
+      });
   }); // almostEqual
 
   it('almostEqualArray on arrays of numbers', () => {
@@ -58,7 +56,8 @@ describe('almostEqual', () => {
 
     fc.assert(
       fc.property(
-        fc.array(fc.double({ noNaN: true, noDefaultInfinity: true })), fc.double({ noNaN: true, noDefaultInfinity: true, min: 0 }),
+        fc.array(fc.double({ noDefaultInfinity: true, noNaN: true })),
+        fc.double({ min: 0, noDefaultInfinity: true, noNaN: true }),
         (value, tolerance) => {
 
           // tolerance should sum all possible computation errors
@@ -67,12 +66,30 @@ describe('almostEqual', () => {
           );
 
           assert(almostEqualArray(value, value.map(v => v + Math.sign(v) * tolerance), 2 * tolerance),
-            `Absolute tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`
+            `Absolute tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
           );
 
           assert(almostEqualArray(value, value.map(v => v * range), 2 * range),
-            `Relative tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`
+            `Relative tolerance too strict: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
           );
+
+          assert(!almostEqualArray(value, value.concat([0])),
+            `Different size arrays should not be equal: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
+          );
+
+          const differentValue = value.map(v => {
+            const dv = Math.sign(v) * Math.max(
+              Math.abs(v + Math.sign(v) * range),
+              Math.abs(v * range),
+            );
+            return (dv === v || Math.abs(dv) === Infinity) ? v : dv;
+          });
+
+          if (differentValue.some((dv, i) => dv !== value[i])) {
+            assert(!almostEqualArray(value, differentValue, 0.5 * range),
+              `Tolerance too relaxed: value: ${value}, tolerance: ${tolerance}, range: ${range}`,
+            );
+          }
 
 
         }), {
